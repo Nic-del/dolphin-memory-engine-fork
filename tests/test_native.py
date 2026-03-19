f5d89317826afbd1f58c5f257ede660f84b32271
NicolasDele <116729273+NicolasDele@users.noreply.github.com>
2026-03-19 09:41:32 +0100

    Correction des tests et version 2.0.1

--- tests/test_native.py
+++ tests/test_native.py
@@ -1,11 +1,11 @@
-import dolphin_memory_engine
+import dolphin_memory_engine_fork
 
 
 def test_is_hooked_default():
-    assert not dolphin_memory_engine.is_hooked()
+    assert not dolphin_memory_engine_fork.is_hooked()
 
 
 def test_hook_unhook():
-    dolphin_memory_engine.hook()
-    dolphin_memory_engine.un_hook()
-    assert not dolphin_memory_engine.is_hooked()
+    dolphin_memory_engine_fork.hook()
+    dolphin_memory_engine_fork.un_hook()
+    assert not dolphin_memory_engine_fork.is_hooked()
